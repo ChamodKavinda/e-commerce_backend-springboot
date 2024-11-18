3f0861cedaa55ce2776c2517de6ad1c949906970
chamod kavinda <kavindachamod100@gmail.com>
2024-11-18 22:40:30 +0530

    first commit

---
+++ src/main/java/com/chamod/ecommerce_backend/model/Address.java
@@ -0,0 +1,65 @@
+package com.chamod.ecommerce_backend.model;
+
+import jakarta.persistence.*;
+
+@Table(name = "address")
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id" , nullable = false)
+    private Long id;
+
+    @Column(name = "address_line_1", nullable = false, length = 522)
+    private String addressLine1;
+
+    @Column(name = "address_line_2", length = 522)
+    private String addressLine2;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "country", nullable = false, length = 60)
+    private String country;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
