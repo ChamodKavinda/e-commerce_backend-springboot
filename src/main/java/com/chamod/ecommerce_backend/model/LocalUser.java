11703d279dcd071d6d63bb02cd32fbdbf709d2a6
chamod kavinda <kavindachamod100@gmail.com>
2024-11-19 10:45:56 +0530

    JoinColumns

--- src/main/java/com/chamod/ecommerce_backend/model/LocalUser.java
+++ src/main/java/com/chamod/ecommerce_backend/model/LocalUser.java
@@ -1,16 +1,25 @@
 package com.chamod.ecommerce_backend.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Table(name = "local_user")
+
 @Entity
+@Table(name = "local_user")
 public class LocalUser {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id" , nullable = false)
+    @Column(name = "id", nullable = false)
     private Long id;
 
     @Column(name = "username", nullable = false, unique = true)
@@ -28,62 +37,77 @@ public class LocalUser {
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE,orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<Address> addresses = new ArrayList<>();
 
-    public List<Address> getAddresses(){
+
+    public List<Address> getAddresses() {
         return addresses;
     }
 
-    public void setAddresses(List<Address> addresses){
-        this.addresses=addresses;
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
     }
+
 
     public String getLastName() {
         return lastName;
     }
 
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
 
     public String getFirstName() {
         return firstName;
     }
 
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
 
     public String getEmail() {
         return email;
     }
 
+
     public void setEmail(String email) {
         this.email = email;
     }
+
 
     public String getPassword() {
         return password;
     }
 
+
     public void setPassword(String password) {
         this.password = password;
     }
+
 
     public String getUsername() {
         return username;
     }
 
+
     public void setUsername(String username) {
         this.username = username;
     }
+
 
     public Long getId() {
         return id;
     }
 
+
     public void setId(Long id) {
         this.id = id;
     }
+
 }
--- src/main/java/com/chamod/ecommerce_backend/model/Product.java
+++ src/main/java/com/chamod/ecommerce_backend/model/Product.java
@@ -1,13 +1,22 @@
 package com.chamod.ecommerce_backend.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
-@Table(name = "product")
+
 @Entity
+@Table(name = "product")
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id" , nullable = false)
+    @Column(name = "id", nullable = false)
     private Long id;
 
     @Column(name = "name", nullable = false, unique = true)
@@ -22,41 +31,64 @@ public class Product {
     @Column(name = "price", nullable = false)
     private Double price;
 
+    @OneToOne(mappedBy = "product", cascade = CascadeType.REMOVE, optional = false, orphanRemoval = true)
+    private Inventory inventory;
+
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+
     public Double getPrice() {
         return price;
     }
+
 
     public void setPrice(Double price) {
         this.price = price;
     }
 
+
     public String getLongDescription() {
         return longDescription;
     }
+
 
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
     }
 
+
     public String getShortDescription() {
         return shortDescription;
     }
+
 
     public void setShortDescription(String shortDescription) {
         this.shortDescription = shortDescription;
     }
 
+
     public String getName() {
         return name;
     }
+
 
     public void setName(String name) {
         this.name = name;
     }
 
+
     public Long getId() {
         return id;
     }
+
 
     public void setId(Long id) {
         this.id = id;
