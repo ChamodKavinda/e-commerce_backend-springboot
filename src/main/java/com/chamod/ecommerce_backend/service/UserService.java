7103aef89654881adfcdee1b193f8a77d073f188
chamod kavinda <kavindachamod100@gmail.com>
2024-11-21 12:28:29 +0530

    implement user login service

--- src/main/java/com/chamod/ecommerce_backend/service/UserService.java
+++ src/main/java/com/chamod/ecommerce_backend/service/UserService.java
@@ -1,20 +1,25 @@
 package com.chamod.ecommerce_backend.service;
 
+import com.chamod.ecommerce_backend.api.model.LoginBody;
 import com.chamod.ecommerce_backend.api.model.RegistrationBody;
 import com.chamod.ecommerce_backend.exception.UserAlreadyExistsException;
 import com.chamod.ecommerce_backend.model.LocalUser;
 import com.chamod.ecommerce_backend.model.dao.LocalUserDAO;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     private LocalUserDAO localUserDAO;
     private EncryptionService encryptionService;
+    private JWTService jwtService;
 
-    public UserService(LocalUserDAO localUserDAO, EncryptionService encryptionService) {
+    public UserService(LocalUserDAO localUserDAO, EncryptionService encryptionService, JWTService jwtService) {
         this.localUserDAO = localUserDAO;
         this.encryptionService = encryptionService;
+        this.jwtService = jwtService;
     }
 
     public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistsException {
@@ -34,6 +39,20 @@ public class UserService {
 
         return localUserDAO.save(user);
 
+    }
+
+
+
+    public String loginUser(LoginBody loginBody){
+        Optional<LocalUser> opUser = localUserDAO.findByUsernameIsIgnoreCase(loginBody.getUsername());
+
+        if(opUser.isPresent()){
+            LocalUser user = opUser.get();
+            if(encryptionService.verifyPassword(loginBody.getPassword(),user.getPassword())){
+                return jwtService.generateJWT(user);
+            }
+        }
+        return null;
     }
 
 
