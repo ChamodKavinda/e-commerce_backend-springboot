1e2bdd12ef7e7f3c9a404e1ede90809e251a44dd
chamod kavinda <kavindachamod100@gmail.com>
2024-11-21 11:33:07 +0530

    pom

--- src/main/java/com/chamod/ecommerce_backend/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/chamod/ecommerce_backend/api/controller/auth/AuthenticationController.java
@@ -5,7 +5,6 @@ import com.chamod.ecommerce_backend.exception.UserAlreadyExistsException;
 import com.chamod.ecommerce_backend.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
