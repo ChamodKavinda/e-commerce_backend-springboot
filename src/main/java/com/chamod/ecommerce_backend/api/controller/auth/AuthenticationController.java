0a109dbb7c9ec3198cc15c0a0663f3d16f6ef117
chamod kavinda <kavindachamod100@gmail.com>
2024-11-29 23:36:58 +0530

    implement JWTRequestFilter class

--- src/main/java/com/chamod/ecommerce_backend/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/chamod/ecommerce_backend/api/controller/auth/AuthenticationController.java
@@ -4,14 +4,13 @@ import com.chamod.ecommerce_backend.api.model.LoginBody;
 import com.chamod.ecommerce_backend.api.model.LoginResponse;
 import com.chamod.ecommerce_backend.api.model.RegistrationBody;
 import com.chamod.ecommerce_backend.exception.UserAlreadyExistsException;
+import com.chamod.ecommerce_backend.model.LocalUser;
 import com.chamod.ecommerce_backend.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -45,5 +44,10 @@ public class AuthenticationController {
             response.setJwt(jwt);
             return ResponseEntity.ok(response);
         }
+    }
+
+    @GetMapping("/me")
+    public LocalUser getLoggedInUserProfile(@AuthenticationPrincipal LocalUser user){
+        return  user;
     }
 }
