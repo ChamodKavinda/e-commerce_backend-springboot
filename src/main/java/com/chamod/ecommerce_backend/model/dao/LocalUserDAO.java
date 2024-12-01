1cf623fdaf500a02021941cb0487686eb2f109f5
chamod kavinda <kavindachamod100@gmail.com>
2024-12-01 10:32:36 +0530

    change repository type

--- src/main/java/com/chamod/ecommerce_backend/model/dao/LocalUserDAO.java
+++ src/main/java/com/chamod/ecommerce_backend/model/dao/LocalUserDAO.java
@@ -1,11 +1,11 @@
 package com.chamod.ecommerce_backend.model.dao;
 
 import com.chamod.ecommerce_backend.model.LocalUser;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.Optional;
 
-public interface LocalUserDAO extends CrudRepository<LocalUser,Long> {
+public interface LocalUserDAO extends ListCrudRepository<LocalUser,Long> {
     Optional<LocalUser> findByUsernameIsIgnoreCase(String username);
 
     Optional<LocalUser> findByEmailIsIgnoreCase(String email);
