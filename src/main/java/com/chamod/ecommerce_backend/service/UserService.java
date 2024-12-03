aa7a5d675598921d48c86ba313d831e476ed5722
chamod kavinda <kavindachamod100@gmail.com>
2024-12-03 21:40:06 +0530

    generate verification Token

--- src/main/java/com/chamod/ecommerce_backend/service/UserService.java
+++ src/main/java/com/chamod/ecommerce_backend/service/UserService.java
@@ -2,11 +2,15 @@ package com.chamod.ecommerce_backend.service;
 
 import com.chamod.ecommerce_backend.api.model.LoginBody;
 import com.chamod.ecommerce_backend.api.model.RegistrationBody;
+import com.chamod.ecommerce_backend.exception.EmailFailureException;
 import com.chamod.ecommerce_backend.exception.UserAlreadyExistsException;
 import com.chamod.ecommerce_backend.model.LocalUser;
+import com.chamod.ecommerce_backend.model.VerificationToken;
 import com.chamod.ecommerce_backend.model.dao.LocalUserDAO;
+import com.chamod.ecommerce_backend.model.dao.VerificationTokenDAO;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.Optional;
 
 @Service
@@ -15,14 +19,17 @@ public class UserService {
     private LocalUserDAO localUserDAO;
     private EncryptionService encryptionService;
     private JWTService jwtService;
+    private EmailService emailService;
+    private VerificationTokenDAO verificationTokenDAO;
 
-    public UserService(LocalUserDAO localUserDAO, EncryptionService encryptionService, JWTService jwtService) {
+    public UserService(LocalUserDAO localUserDAO, EncryptionService encryptionService, JWTService jwtService, VerificationTokenDAO verificationTokenDAO) {
         this.localUserDAO = localUserDAO;
         this.encryptionService = encryptionService;
         this.jwtService = jwtService;
+        this.verificationTokenDAO = verificationTokenDAO;
     }
 
-    public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistsException {
+    public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistsException, EmailFailureException {
 
         if (localUserDAO.findByEmailIsIgnoreCase(registrationBody.getEmail()).isPresent()
                 || localUserDAO.findByUsernameIsIgnoreCase(registrationBody.getUsername()).isPresent()){
@@ -34,9 +41,7 @@ public class UserService {
         user.setFirstName(registrationBody.getFirstName());
         user.setLastName(registrationBody.getLastName());
         user.setUsername(registrationBody.getUsername());
-
         user.setPassword(encryptionService.encryptPassword(registrationBody.getPassword()));
-
         return localUserDAO.save(user);
 
     }
@@ -53,6 +58,15 @@ public class UserService {
             }
         }
         return null;
+    }
+
+    private VerificationToken createVerificationToken(LocalUser user){
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(jwtService.generateVerificationJWT(user));
+        verificationToken.setCreatedTimestamp(new Timestamp(System.currentTimeMillis()));
+        verificationToken.setUser(user);
+        user.getVerificationTokens().add(verificationToken);
+        return verificationToken;
     }
 
 
