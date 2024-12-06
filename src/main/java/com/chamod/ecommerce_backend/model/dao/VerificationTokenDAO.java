1e73b26b00dba017a56d45927e4eabfc42758a0f
chamod kavinda <kavindachamod100@gmail.com>
2024-12-06 19:05:24 +0530

    implement Custom Query Methods

--- src/main/java/com/chamod/ecommerce_backend/model/dao/VerificationTokenDAO.java
+++ src/main/java/com/chamod/ecommerce_backend/model/dao/VerificationTokenDAO.java
@@ -1,7 +1,13 @@
 package com.chamod.ecommerce_backend.model.dao;
 
+import com.chamod.ecommerce_backend.model.LocalUser;
 import com.chamod.ecommerce_backend.model.VerificationToken;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Optional;
+
 public interface VerificationTokenDAO extends ListCrudRepository<VerificationToken,Long> {
+    Optional<VerificationToken> findByToken(String token);
+
+    void deleteByUser(LocalUser user);
 }
