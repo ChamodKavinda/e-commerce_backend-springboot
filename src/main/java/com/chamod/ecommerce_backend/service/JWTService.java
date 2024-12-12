1303b9b3c516fb2cd17874c2ae79904db36049a9
chamod kavinda <kavindachamod100@gmail.com>
2024-12-12 21:54:00 +0530

    verify jwt token

--- src/main/java/com/chamod/ecommerce_backend/service/JWTService.java
+++ src/main/java/com/chamod/ecommerce_backend/service/JWTService.java
@@ -2,6 +2,7 @@ package com.chamod.ecommerce_backend.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.chamod.ecommerce_backend.model.LocalUser;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,11 +27,7 @@ public class JWTService {
 
     @PostConstruct
     public void postConstruct(){
-        try {
-            algorithm = Algorithm.HMAC256(algorithmKey);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        algorithm = Algorithm.HMAC256(algorithmKey);
     }
 
     public String generateJWT(LocalUser user){
@@ -50,7 +47,8 @@ public class JWTService {
     }
 
     public String getUsername(String token){
-        return JWT.decode(token).getClaim(USERNAME_KEY).asString();
+        DecodedJWT jwt = JWT.require(algorithm).withIssuer(issuer).build().verify(token);
+        return jwt.getClaim(USERNAME_KEY).asString();
     }
 
 }
