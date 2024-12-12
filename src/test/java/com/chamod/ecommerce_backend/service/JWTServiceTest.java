c8df0fa049b57b138d726839eb108eedd39d2416
chamod kavinda <kavindachamod100@gmail.com>
2024-12-12 21:31:09 +0530

    test fakeJWT token

--- src/test/java/com/chamod/ecommerce_backend/service/JWTServiceTest.java
+++ src/test/java/com/chamod/ecommerce_backend/service/JWTServiceTest.java
@@ -1,12 +1,17 @@
 package com.chamod.ecommerce_backend.service;
 
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.chamod.ecommerce_backend.model.LocalUser;
 import com.chamod.ecommerce_backend.model.dao.LocalUserDAO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.UnsupportedEncodingException;
 
 
 @SpringBootTest
@@ -33,6 +38,15 @@ public class JWTServiceTest {
         LocalUser user = localUserDAO.findByUsernameIgnoreCase("UserA").get();
         String token = jwtService.generateJWT(user);
         Assertions.assertEquals(user.getUsername(), jwtService.getUsername(token), "Token for auth should contain users username.");
+    }
+
+    @Test
+    public void testJWTNotGeneratedByUs() throws UnsupportedEncodingException {
+        String token =
+                JWT.create().withClaim("USERNAME", "UserA").sign(Algorithm.HMAC256(
+                        "NotTheRealSecret"));
+        Assertions.assertThrows(SignatureVerificationException.class,
+                () -> jwtService.getUsername(token));
     }
 
 }
