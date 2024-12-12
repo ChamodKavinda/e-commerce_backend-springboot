32b29a57a492a4f806958afc2dc5b94a4e1bc90b
chamod kavinda <kavindachamod100@gmail.com>
2024-12-12 20:07:45 +0530

    test unauthenticate Request

---
+++ src/test/java/com/chamod/ecommerce_backend/security/JWTRequestFilterTest.java
@@ -0,0 +1,30 @@
+package com.chamod.ecommerce_backend.security;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JWTRequestFilterTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+
+    private static final String AUTHENTICATED_PATH = "/auth/me";
+
+
+    @Test
+    public void testUnauthenticatedRequest() throws Exception {
+        mvc.perform(get(AUTHENTICATED_PATH)).andExpect(status().is(HttpStatus.FORBIDDEN.value()));
+    }
+
+}
