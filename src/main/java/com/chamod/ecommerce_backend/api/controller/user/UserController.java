98aa99564b4ef87586642b718395c6c6802bc455
chamod kavinda <kavindachamod100@gmail.com>
2024-12-14 21:39:12 +0530

    edit user address function

--- src/main/java/com/chamod/ecommerce_backend/api/controller/user/UserController.java
+++ src/main/java/com/chamod/ecommerce_backend/api/controller/user/UserController.java
@@ -6,10 +6,7 @@ import com.chamod.ecommerce_backend.model.dao.AddressDAO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +27,20 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
         return ResponseEntity.ok(addressDAO.findByUser_Id(userId));
+    }
+
+    @PutMapping("/{userId}/address")
+    public ResponseEntity<Address> putAddress(
+            @AuthenticationPrincipal LocalUser user, @PathVariable Long userId,
+            @RequestBody Address address) {
+        if (!userHasPermission(user, userId)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        address.setId(null);
+        LocalUser refUser = new LocalUser();
+        refUser.setId(userId);
+        address.setUser(refUser);
+        return ResponseEntity.ok(addressDAO.save(address));
     }
 
     private boolean userHasPermission(LocalUser user, Long id) {
